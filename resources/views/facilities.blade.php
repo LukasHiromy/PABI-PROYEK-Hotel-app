6b7cbe264f2acb2896605ff88a9fb5b2d8339964
sintadamesinaga <131933461+sintadamesinaga@users.noreply.github.com>
2023-05-11 11:17:38 +0700

    untuk fasilitas
    menambahkan halaman dari fasilitas

---
+++ resources/views/facilities.blade.php
@@ -0,0 +1,64 @@
+<body class="bg-light">
+  <nav class="navbar navbar-expand-lg navbar-light bg-white px-lg-3 py-lg-2 shadow-sm sticky-top">
+    <!-- kode program navbar di atas -->
+  </nav>
+  <div class="container my-5">
+    <h2 class="text-center mb-4">Facilities</h2>
+    <div class="row">
+      <div class="col-lg-8 offset-lg-2">
+        <table class="table table-striped facilities">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Facility Name</th>
+              <th scope="col">Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Kolam renang</td>
+              <td>Terletak di lantai atas hotel dengan pemandangan yang indah</td>
+              <img src="img_1.jpg" alt="Gambar hotel dengan kolam renang di lantai atas">
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Spa</td>
+              <td>Menawarkan berbagai perawatan tubuh dan kecantikan</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>Gym</td>
+              <td>Dilengkapi dengan alat-alat fitness modern dan instruktur profesional</td>
+            </tr>
+            <tr>
+              <th scope="row">4</th>
+              <td>Restoran</td>
+              <td>Menyajikan masakan internasional dan lokal dengan citarasa yang lezat</td>
+            </tr>
+            <tr>
+              <th scope="row">5</th>
+              <td>Bar</td>
+              <td>Menyediakan berbagai minuman dan camilan ringan</td>
+            </tr>
+            <tr>
+              <th scope="row">6</th>
+              <td>Ruang pertemuan</td>
+              <td>Dapat menampung hingga 100 orang untuk berbagai acara bisnis atau pribadi</td>
+            </tr>
+            <tr>
+              <th scope="row">7</th>
+              <td>Layanan kamar</td>
+              <td>Tersedia 24 jam dengan pilihan menu yang beragam</td>
+            </tr>
+            <tr>
+              <th scope="row">8</th>
+              <td>Wi-Fi gratis</td>
+              <td>Tersedia di seluruh area hotel dan kamar</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</body>
---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,46 @@
++<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hotel Facilities</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Hotel</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Facilities <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Rooms</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <header class="bg-primary text-white py-5">
+        <div class="container">
+            <h1 class="display-4">Hotel Facilities</h1>
+        </div>
+    </header>
+
+
+    <!-- jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+</html>
