24b95327a9dcab743734cb3169e58be38b32da6e
LukasHiromy <lukassimatupang@gmail.com>
2023-05-23 16:59:35 +0700

    membuat admin manage & tabel Room Beserta relation

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,16 @@ use App\Http\Controllers\PageController;
 //     return view('welcome');
 // });
 
+//Admin Dashboard
+Route::get('admin', function(){
+    return view('admin.admin');
+});
+
+//user dashboard
 Route::post('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
+
+//login
 Route::get('auth/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('auth/login', [AuthController::class, 'login']);
 Route::get('auth/register', [AuthController::class, 'showRegistrationForm'])->name('register');
@@ -29,7 +37,6 @@ Route::post('auth/register', [AuthController::class, 'register']);
 Route::post('auth/logout', [AuthController::class, 'logout'])->name('logout');
 
 
-Route::get('/', [PageController::class, 'index'])->name('home');
 Route::get('/', [PageController::class, 'index'])->name('home');
 Route::get('/rooms', [PageController::class, 'rooms']);
 Route::get('/facilities', [PageController::class, 'facilities']);
