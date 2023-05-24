b98f8cdef82e188dbd2744f5e8e823a2cf4cb3c5
Velicia Chinnara <131932993+veliciachinnara@users.noreply.github.com>
2023-05-24 21:40:33 +0700

    Update rooms.blade.php

--- resources/views/page/rooms.blade.php
+++ resources/views/page/rooms.blade.php
@@ -7,15 +7,18 @@
 <h2 class="mt-3 pt-2 mb-2 text-center fw-bold h-font">Our Rooms</h2>
 <div class="container">
   <div class="row">
-
+  
   <!-- Room card 1-->
-    <div class="col-lg-4 co-md-6 my-3">
-      <div class="card border-0 shadow" style="max-width: 350px; margin:auto">
+    <div class="col-lg-4 co-md-4 my-3">
+      <div class="card border-3 shadow" style="max-width: 450px; margin:auto">
         <img src="images/rooms/1.jpg" class="card-img-top">  
         <div class="card-body">
           <h5>Tower Suite</h5>
           <h6 class="mb-4">IDR200.000 per night</h6>
-          <div class="features mb-4">
+          <div class="rating mb-4">
+            <h6 class="mb-1">2-4 people</h6>
+          </div>
+          <div class="features mb-2">
             <h6 class="mb-1">Features</h6>
             <span class="badge rounded-pill bg-light text-dark text-wrap"> a sofa bed</span>
             <span class="badge rounded-pill bg-light text-dark text-wrap">a fully equipped kitchen </span>
@@ -29,7 +32,7 @@
           </div>
 
           <div class="facilities mb-4">
-          <h6 class="mb-1">Facilities</h6>
+          <h6 class="mb-0">Facilities</h6>
           <span class="badge rounded-pill bg-light text-dark text-wrap">Wi-Fi</span>
           <span class="badge rounded-pill bg-light text-dark text-wrap">TV</span>
           <span class="badge rounded-pill bg-light text-dark text-wrap">Radio</span>
@@ -53,12 +56,15 @@
     </div>
 
      <!-- Room card 2-->
-     <div class="col-lg-4 co-md-6 my-3">
-      <div class="card border-0 shadow" style="max-width: 350px; margin:auto">
-        <img src="images/rooms/1.jpg" class="card-img-top">  
+     <div class="col-lg-4 co-md-4 my-3">
+      <div class="card border-3 shadow" style="max-width: 450px; margin:auto">
+        <img src="images/rooms/3.png" class="card-img-top">  
         <div class="card-body">
           <h5>Sky Suite Palais</h5>
           <h6 class="mb-4">IDR200.000 per night</h6>
+          <div class="rating mb-4">
+            <h6 class="mb-1">2-4 people</h6>
+          </div>
           <div class="features mb-4">
             <h6 class="mb-1">Features</h6>
             <span class="badge rounded-pill bg-light text-dark text-wrap">one bedroom </span>
@@ -71,7 +77,7 @@
           <div class="facilities mb-4">
           <h6 class="mb-1">Facilities</h6>
           <span class="badge rounded-pill bg-light text-dark text-wrap">Wi-Fi</span>
-          <span class="badge rounded-pill bg-light text-dark text-wrap">TV</span>
+          <span class="badge rounded-pill bg-light text-dark text-wrap">HD-flat screen TV</span>
           <span class="badge rounded-pill bg-light text-dark text-wrap">Radio</span>
           </div>
           
@@ -84,6 +90,8 @@
               <i class="bi bi-star-fill text-warning"></i>
             </span>
           </div>
+
+          <br>
           <div class="d-flex justify-content-evenly mb-2">
           <a href="#" class="btn btn-sm text-white custom-bg shadow-none">Book Now</a>
           <a href="#" class="btn btn-sm btn-outline-dark shadow-none">More details</a>
@@ -93,9 +101,9 @@
     </div>
 
      <!-- Room card 3-->
-     <div class="col-lg-4 co-md-6 my-3">
-      <div class="card border-0 shadow" style="max-width: 350px; margin:auto">
-        <img src="images/rooms/1.jpg" class="card-img-top">  
+     <div class="col-lg-4 co-md-4 my-3">
+      <div class="card border-3 shadow" style="max-width: 450px; margin:auto">
+        <img src="images/rooms/3.png" class="card-img-top">  
         <div class="card-body">
           <h5>2-room Suite Palais</h5>
           <h6 class="mb-4">IDR200.000 per night</h6>
@@ -106,15 +114,24 @@
             <span class="badge rounded-pill bg-light text-dark text-wrap">separate children’s room</span>
             <span class="badge rounded-pill bg-light text-dark text-wrap">bath with shower/WC</span>
             <span class="badge rounded-pill bg-light text-dark text-wrap">bathroom scales</span>
-            <span class="badge rounded-pill bg-light text-dark text-wrap">sauna towels</span>
+            <span class="badge rounded-pill bg-light text-dark text-wrap">mini bar</span>
+            <span class="badge rounded-pill bg-light text-dark text-wrap">convertible sofa</span>
+            <span class="badge rounded-pill bg-light text-dark text-wrap">cosy sitting area</span>
+            <span class="badge rounded-pill bg-light text-dark text-wrap">balcony</span>
+            
           </div>
 
           <div class="facilities mb-4">
           <h6 class="mb-1">Facilities</h6>
-          <span class="badge rounded-pill bg-light text-dark text-wrap">Wifi</span>
-          <span class="badge rounded-pill bg-light text-dark text-wrap">Wifi</span>
-          <span class="badge rounded-pill bg-light text-dark text-wrap">Wifi</span>
-          <span class="badge rounded-pill bg-light text-dark text-wrap">Wifi</span>
+          <span class="badge rounded-pill bg-light text-dark text-wrap">cosmetics</span>
+          <span class="badge rounded-pill bg-light text-dark text-wrap">HD-flat screen TV</span>
+          <span class="badge rounded-pill bg-light text-dark text-wrap">Wi-Fi</span>
+          <span class="badge rounded-pill bg-light text-dark text-wrap">radio</span>
+          <span class="badge rounded-pill bg-light text-dark text-wrap">bathrobes</span>
+          <span class="badge rounded-pill bg-light text-dark text-wrap">sauna towels</span>
+          <span class="badge rounded-pill bg-light text-dark text-wrap">slippers</span>
+          <span class="badge rounded-pill bg-light text-dark text-wrap">safe</span>
+          <span class="badge rounded-pill bg-light text-dark text-wrap">water kettles</span>
           </div>
           
           <div class="rating mb-4">
@@ -135,29 +152,41 @@
     </div>
 
      <!-- Room card 4-->
-     <div class="col-lg-4 co-md-6 my-3">
-      <div class="card border-0 shadow" style="max-width: 350px; margin:auto">
-        <img src="images/rooms/1.jpg" class="card-img-top">  
+     <div class="col-lg-4 co-md-4 my-3">
+      <div class="card border-3 shadow" style="max-width: 450px; margin:auto">
+        <img src="images/rooms/4.png" class="card-img-top">  
         <div class="card-body">
           <h5>1-room Suite Palais</h5>
           <h6 class="mb-4">IDR200.000 per night</h6>
           <div class="features mb-4">
             <h6 class="mb-1">Features</h6>
-            <span class="badge rounded-pill bg-light text-dark text-wrap">bath/shower</span>
-            <span class="badge rounded-pill bg-light text-dark text-wrap">separate toilet</span>
-            <span class="badge rounded-pill bg-light text-dark text-wrap">scales</span>
+            <span class="badge rounded-pill bg-light text-dark text-wrap">Large sleeping & living room</span>
+            <span class="badge rounded-pill bg-light text-dark text-wrap">bath with shower/WC</span>
+            <span class="badge rounded-pill bg-light text-dark text-wrap">bathroom scales</span>
             <span class="badge rounded-pill bg-light text-dark text-wrap">safe and cozy sitting area</span>
-            <span class="badge rounded-pill bg-light text-dark text-wrap">hairdryer</span>
-            
-          </div>
-          <div class="facilities mb-4">
-          <h6 class="mb-1">Facilities</h6>
-          <span class="badge rounded-pill bg-light text-dark text-wrap">direct dial telephone</span>
-          <span class="badge rounded-pill bg-light text-dark text-wrap">flat screen TV</span>
-          <span class="badge rounded-pill bg-light text-dark text-wrap"> radio</span>
-          <span class="badge rounded-pill bg-light text-dark text-wrap">wireless</span>
+            <span class="badge rounded-pill bg-light text-dark text-wrap">hairdryer</span>  
+            <span class="badge rounded-pill bg-light text-dark text-wrap">mini bar</span>
+            <span class="badge rounded-pill bg-light text-dark text-wrap">convertible sofa</span>
+            <span class="badge rounded-pill bg-light text-dark text-wrap">balcony</span>
+            <span class="badge rounded-pill bg-light text-dark text-wrap">cosy sitting area with a desk</span>
           
           </div>
+
+          <div class="facilities mb-4">
+          <h6 class="mb-1">Facilities</h6>
+          <span class="badge rounded-pill bg-light text-dark text-wrap">cosmetics</span>
+          <span class="badge rounded-pill bg-light text-dark text-wrap">HD-flat screen TV</span>
+          <span class="badge rounded-pill bg-light text-dark text-wrap">Wi-Fi</span>
+          <span class="badge rounded-pill bg-light text-dark text-wrap">radio</span>
+          <span class="badge rounded-pill bg-light text-dark text-wrap">bathrobes</span>
+          <span class="badge rounded-pill bg-light text-dark text-wrap">sauna towels</span>
+          <span class="badge rounded-pill bg-light text-dark text-wrap">slippers</span>
+          <span class="badge rounded-pill bg-light text-dark text-wrap">safe</span>
+          <span class="badge rounded-pill bg-light text-dark text-wrap">hair dryer</span>
+          <span class="badge rounded-pill bg-light text-dark text-wrap"> free WLAN</span>
+          <span class="badge rounded-pill bg-light text-dark text-wrap">water kettles</span>
+          </div>
+
           <div class="rating mb-4">
             <h6 class="mb-1">Rating</h6>
             <span class="badge rounded-pill bg-light">
@@ -175,25 +204,32 @@
     </div>
 
      <!-- Room card 5-->
-     <div class="col-lg-4 co-md-6 my-3">
-      <div class="card border-0 shadow" style="max-width: 350px; margin:auto">
-        <img src="images/rooms/1.jpg" class="card-img-top">  
+     <div class="col-lg-4 co-md-4 my-3">
+      <div class="card border-3 shadow" style="max-width: 450px; margin:auto">
+        <img src="images/rooms/5.png" class="card-img-top">  
         <div class="card-body">
           <h5>1-Room Suite</h5>
           <h6 class="mb-4">IDR200.000 per night</h6>
           <div class="features mb-4">
             <h6 class="mb-1">Features</h6>
-            <span class="badge rounded-pill bg-light text-dark text-wrap">2 Rooms</span>
-            <span class="badge rounded-pill bg-light text-dark text-wrap">2 Rooms</span>
-            <span class="badge rounded-pill bg-light text-dark text-wrap">2 Rooms</span>
+            <span class="badge rounded-pill bg-light text-dark text-wrap">Large sleeping & living room</span>
+            <span class="badge rounded-pill bg-light text-dark text-wrap">bath with shower/WC</span>
+            <span class="badge rounded-pill bg-light text-dark text-wrap">bathroom scales</span>
           </div>
 
           <div class="facilities mb-4">
           <h6 class="mb-1">Facilities</h6>
-          <span class="badge rounded-pill bg-light text-dark text-wrap">Wifi</span>
-          <span class="badge rounded-pill bg-light text-dark text-wrap">Wifi</span>
-          <span class="badge rounded-pill bg-light text-dark text-wrap">Wifi</span>
-          <span class="badge rounded-pill bg-light text-dark text-wrap">Wifi</span>
+          <span class="badge rounded-pill bg-light text-dark text-wrap">cosmetics</span>
+          <span class="badge rounded-pill bg-light text-dark text-wrap">HD-flat screen TV</span>
+          <span class="badge rounded-pill bg-light text-dark text-wrap">Wi-Fi</span>
+          <span class="badge rounded-pill bg-light text-dark text-wrap">radio</span>
+          <span class="badge rounded-pill bg-light text-dark text-wrap">bathrobes</span>
+          <span class="badge rounded-pill bg-light text-dark text-wrap">sauna towels</span>
+          <span class="badge rounded-pill bg-light text-dark text-wrap">slippers</span>
+          <span class="badge rounded-pill bg-light text-dark text-wrap">safe</span>
+          <span class="badge rounded-pill bg-light text-dark text-wrap">hair dryer</span>
+          <span class="badge rounded-pill bg-light text-dark text-wrap"> free WLAN</span>
+          <span class="badge rounded-pill bg-light text-dark text-wrap">water kettles</span>
           </div>
           
           <div class="rating mb-4">
@@ -215,16 +251,16 @@
 
 
      <!-- Room card 6-->
-    <div class="col-lg-4 co-md-6 my-3">
-      <div class="card border-0 shadow" style="max-width: 350px; margin:auto">
-        <img src="images/rooms/1.jpg" class="card-img-top">  
+     <div class="col-lg-4 co-md-4 my-3">
+      <div class="card border-3 shadow" style="max-width: 450px; margin:auto">
+        <img src="images/rooms/6.png" class="card-img-top">  
         <div class="card-body">
           <h5>Double room Palais</h5>
           <h6 class="mb-4">IDR200.000 per night</h6>
           <div class="features mb-4">
             <h6 class="mb-1">Features</h6>
-            <span class="badge rounded-pill bg-light text-dark text-wrap">2 Rooms</span>
-            <span class="badge rounded-pill bg-light text-dark text-wrap">2 Rooms</span>
+            <span class="badge rounded-pill bg-light text-dark text-wrap">Double room</span>
+            <span class="badge rounded-pill bg-light text-dark text-wrap">bath with shower/WC</span>
             <span class="badge rounded-pill bg-light text-dark text-wrap">2 Rooms</span>
           </div>
 
